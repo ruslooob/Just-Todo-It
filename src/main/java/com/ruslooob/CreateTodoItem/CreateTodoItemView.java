89b44ff5997ae9f11048f47bd60f141ebd89a4db
ruslooob <rm952576@gmail.com>
2022-09-08 22:47:56 +0300

    Remove unused jackson-databind dependency. Fix CreateTodoItemView appearance to more minimalistic

--- src/main/java/com/ruslooob/CreateTodoItem/CreateTodoItemView.java
+++ src/main/java/com/ruslooob/CreateTodoItem/CreateTodoItemView.java
@@ -3,6 +3,7 @@ package com.ruslooob.CreateTodoItem;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -14,8 +15,8 @@ import static com.ruslooob.LangLoader.$;
 public class CreateTodoItemView {
 
     private final Region view;
-    TextField header = new TextField("");
-    TextField content = new TextField("");
+    TextField header;
+    TextArea content;
     Button saveButton = new Button($("create_todo_save_button_text"));
 
     public CreateTodoItemView() {
@@ -25,12 +26,30 @@ public class CreateTodoItemView {
     private Region create() {
         VBox container = vbox(
                 15,
-                hbox(10, new Label($("create_todo_header_label_text")), header),
-                hbox(10, new Label($("create_todo_content_label_text")), content),
+                hbox(10, getHeader()),
+                hbox(10, getContent()),
                 saveButton
         );
-        container.setPadding(new Insets(20));
+        container.setPadding(new Insets(10));
         return container;
+    }
+
+    private TextField getHeader() {
+        if (header == null) {
+            header = new TextField("");
+            header.setPrefWidth(250);
+        }
+        return header;
+    }
+
+    private TextArea getContent() {
+        if (content == null) {
+            content = new TextArea("");
+            content.setPrefWidth(250);
+            content.setPrefHeight(100);
+            content.setWrapText(true);
+        }
+        return content;
     }
 
     public Region get() {
